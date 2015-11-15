df6ea0c8ea902a38690b2701f6b95f9d6be4e7a8
Vishnu karthik <vishnukl@usc.edu>
2015-11-14 19:49:12 -0800

    basic auth

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,24 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('User', new Schema({
-    name: String,
-    password: String
-}));
+var userSchema = mongoose.Schema({
+    email: String,
+    password: String,
+    token: String
+});
+
+userSchema.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.methods.safeData = function () {
+    return {email: this.email, token: this.token}
+};
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -3,17 +3,23 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt-nodejs');
 var config = require('./config');
 var User = require('./app/models/user');
 
 var port = process.env.PORT || 8080;
 mongoose.connect(config.database);
 app.set('superSecret', config.secret);
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
 
 app.get('/', function (req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
@@ -22,3 +28,103 @@ app.get('/', function (req, res) {
 
 app.listen(port);
 console.log('Magic happens at http://localhost:' + port);
+
+var apiRoutes = express.Router();
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+app.post('/authenticate', function (req, res) {
+    User.findOne({
+        email: req.body.email
+    }, function (err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            if (user && user.validPassword(req.body.password)) {
+                res.json({
+                    type: true,
+                    data: user.safeData(),
+                    token: user.token
+                });
+            } else {
+                res.json({
+                    type: false,
+                    data: "Incorrect email/password"
+                });
+            }
+        }
+    });
+});
+
+app.post('/signup', function (req, res) {
+    User.findOne({
+        email: req.body.email
+    }, function (err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            if (user) {
+                res.json({
+                    type: false,
+                    data: "User already exists!"
+                });
+            } else {
+                var userModel = new User();
+                userModel.email = req.body.email;
+                userModel.password = userModel.generateHash(req.body.password);
+                userModel.save(function (err, user) {
+                    user.token = jwt.sign(user, process.env.JWT_SECRET);
+                    user.save(function (err, savedUser) {
+                        res.json({
+                            type: true,
+                            data: savedUser.safeData(),
+                            token: savedUser.token
+                        });
+                    });
+                })
+            }
+        }
+    });
+});
+
+function ensureAuthorized(req, res, next) {
+    var bearerToken;
+    var bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        var bearer = bearerHeader.split(" ");
+        bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.send(403);
+    }
+}
+
+app.get('/me', ensureAuthorized, function (req, res) {
+    User.findOne({token: req.token}, function (err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            res.json({
+                type: true,
+                data: user.safeData()
+            });
+        }
+    });
+});
+
+
+apiRoutes.get('/', function (req, res) {
+    res.json({message: 'This is where the hackers are'});
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
